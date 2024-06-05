bd1e2e7b3b641c410e9db71678f1ec18ae6738d2
blueg0 <ghaffour2002@gmail.com>
2024-06-05 20:42:32 +0100

    last

--- SIM808-UP.ino
+++ SIM808-UP.ino
@@ -5,10 +5,11 @@
 #include <PubSubClient.h>
 
 // Constants
-const char* BROKER = "0.tcp.eu.ngrok.io";
-const int MQTT_PORT = 14965;
-const char* TOPIC_GPS = "proj2.0/gps";
-const char* AUTHORIZED_NUMBER = "0556110363";
+const char BROKER[] = "5.tcp.eu.ngrok.io";
+const int MQTT_PORT = 18430;
+const char TOPIC_GPS[] = "proj2.0/gps";
+const char AUTHORIZED_NUMBER[] = "0792449708";
+const int maxGPSData = 10; // Define maximum GPS data storage
 
 // Objects
 SoftwareSerial sim808Serial(10, 11);
@@ -20,7 +21,6 @@ PubSubClient mqtt(client);
 // Variables
 unsigned long lastPublishTime = 0;
 const unsigned long publishInterval = 20000; // 20 seconds
-const int maxGPSData = 10;
 String gpsDataArray[maxGPSData];
 int gpsDataCount = 0;
 
@@ -28,11 +28,10 @@ int gpsDataCount = 0;
 void setupSIM808();
 void setupInternet();
 boolean mqttConnect();
+void storeGPSData();
 void publishGPSData();
 void checkIncomingCall();
 void sendATCommand(const char* cmd);
-String GPSData();
-
 
 //setup
 void setup() {
@@ -40,72 +39,72 @@ void setup() {
   Serial.begin(115200);
   sim808Serial.begin(9600);
   Serial.println("Initializing...");
-  setupSIM808(); //init sim808 and attach gps
-  setupInternet(); //configure gprs and setup connection
+  setupSIM808();    //init sim808 and attach gps
+  setupInternet();  //configure gprs and setup connection
 }
 
 
 //loop
 void loop() {
-  // Publish GPS data at the specified interval
+
+  //Publish GPS data at the specified interval
   unsigned long currentMillis = millis();
+
+
   if (currentMillis - lastPublishTime >= publishInterval) {
     lastPublishTime = currentMillis;
-    if (sim808.getGPS()) {
-      if (gpsDataCount >= maxGPSData) { // If the list is full, remove the oldest data (at index 0)
-        for (int i = 0; i < gpsDataCount - 1; i++) {
-          gpsDataArray[i] = gpsDataArray[i + 1];
-        }
-        gpsDataCount--;
+    Serial.println(F("Checking GPS data..."));
+    while (true) {
+      if (sim808.getGPS()) {
+        break;
       }
+    }
 
-      // Add new GPS data to the array
-      gpsDataArray[gpsDataCount] = GPSData();
-      gpsDataCount++;
-    }
-  
-    //publish the data in the array if the connection established
-    if (gpsDataCount > 0) {
-      publishGPSData();
-    }
+
+    Serial.println(F("GPS data obtained"));
+    storeGPSData();
   }
-    
+    //publish the data in the array if the connection established
+  if (gpsDataCount > 0) {
+    publishGPSData();
+  }
+
+  delay(1500);
   // Check for incoming calls
-  delay(2000);
-  checkIncomingCall();
-}
+  checkIncomingCall();  
+  }
+
+
 
 void setupSIM808() {
-  //check if sim808 is on
   if (!sim808.init()) {
-    Serial.println("SIM808 initialization failed");
-    while (true); 
+    Serial.println(F("SIM808 initialization failed"));
+    while (true); // Stop further execution
   }
-  Serial.println("SIM808 initialized successfully");
-  //check if sim808 attached gps
+  Serial.println(F("SIM808 initialized successfully"));
+
   if (!sim808.attachGPS()) {
-    Serial.println("Failed to enable GPS");
-    while (true); 
+    Serial.println(F("Failed to enable GPS"));
+    while (true); // Stop further execution
   }
-  Serial.println("GPS enabled");
+  Serial.println(F("GPS enabled"));
 }
 
-void setupInternet() {
-  Serial.println("Configuring internet access...");
-
+void setupInternet() {  
+  Serial.println(F("Configuring internet access..."));
   sendATCommand("AT");
-  sendATCommand("AT+CGATT=1"); //enable the GPRS functionality
-  sendATCommand("AT+CSTT=\"internet\""); //set the apn ("internet" is ooredoo apn)
-  sendATCommand("AT+CIICR");//establish the wireless link layer of the GPRS connection after setting the APN
-  sendATCommand("AT+CIFSR");//request the ip address
+  sendATCommand("AT+CGATT=1");            //enable the GPRS functionality
+  sendATCommand("AT+CSTT=\"internet\"");  //set the apn ("internet" is ooredoo apn)
+  sendATCommand("AT+CIICR");              //establish the wireless link layer of the GPRS connection after setting the APN
+  sendATCommand("AT+CIFSR");              //request the ip address
 
   //test the connection
-  Serial.print("Waiting for network...");
+   Serial.print(F("Waiting for network..."));
   if (!modem.waitForNetwork()) {
-    Serial.println(" fail");
+    Serial.println(F(" fail"));
     while (true);
   }
-  Serial.println(" OK");
+  Serial.println(F(" OK"));
 
   delay(3000);
 
@@ -116,77 +115,69 @@ void setupInternet() {
 
 //check if connection to broker is established
 boolean mqttConnect() {
-  Serial.print("Connecting to MQTT broker: ");
+  Serial.print(F("Connecting to MQTT broker: "));
   Serial.print(BROKER);
+
   if (!mqtt.connect("client")) {
-    Serial.println(" Connection failed");
+    Serial.println(F(" Connection failed"));
     return false;
   }
-  Serial.println("Connected to MQTT broker");
+
+  Serial.println(F("Connected to MQTT broker"));
   return mqtt.connected();
 }
 
 
 
-String GPSData() {
-  //Get the 3D data and speed
+void storeGPSData() {
+  if (gpsDataCount >= maxGPSData) {
+    for (int i = 0; i < maxGPSData - 1; i++) {
+      gpsDataArray[i] = gpsDataArray[i + 1]; // Shift data to remove the oldest
+    }
+    gpsDataCount--;
+  }
+
   float lat = sim808.GPSdata.lat;
-  float lon = sim808.GPSdata.lon;
+  float lon = -sim808.GPSdata.lon;
   float speed = sim808.GPSdata.speed_kph;
-  float heading =sim808.GPSdata.heading;
-  String timestamp = String(sim808.GPSdata.day) + "/" +
-                     String(sim808.GPSdata.month) + "/" +
-                     String(sim808.GPSdata.year) + " " +
-                     String(sim808.GPSdata.hour) + ":" +
-                     String(sim808.GPSdata.minute) + ":" +
-                     String(sim808.GPSdata.second);
+  float heading = sim808.GPSdata.heading;
+  char timestamp[25];
+  snprintf(timestamp, sizeof(timestamp), "%02d/%02d/%04d %02d:%02d:%02d", 
+           sim808.GPSdata.day, sim808.GPSdata.month, sim808.GPSdata.year, 
+           sim808.GPSdata.hour, sim808.GPSdata.minute, sim808.GPSdata.second);
 
+  Serial.print(F("Timestamp: ")); Serial.println(timestamp);
+  Serial.print(F("Latitude: ")); Serial.println(lat, 6);
+  Serial.print(F("Longitude: ")); Serial.println(lon, 6);
+  Serial.print(F("Speed (kph): ")); Serial.println(speed);
+  Serial.print(F("Heading: ")); Serial.println(heading);
 
-  //print to serial monitor to check 
-  Serial.print("Timestamp: ");
-  Serial.println(timestamp);
-  Serial.print("Latitude: ");
-  Serial.println(lat, 6);
-  Serial.print("Longitude: ");
-  Serial.println(lon, 6);
-  Serial.print("Speed (kph): ");
-  Serial.println(speed);
-  Serial.print("Heading: ");
-  Serial.println(heading);
+  char message[100];
+  snprintf(message, sizeof(message), "%.6f,%.6f %s %.6f %.6f", lat, lon, timestamp, speed, heading);
 
-  return String(lat, 6) + "," + String(lon, 6) + " " + timestamp + " " + String(speed, 6) + " " + String(heading, 6);
+  gpsDataArray[gpsDataCount] = message;
+  gpsDataCount++;
 }
+
+
 
 void publishGPSData() {
-  if (!mqtt.connected()) {
-    //if not connection lost then reconnect
-    if (mqttConnect()) {
-      if (gpsDataCount > 0) {
-        //if there is data to publis or we are out of interval
-        String message = gpsDataArray[0];
-        mqtt.publish(TOPIC_GPS, message.c_str());
-        for (int i = 0; i < gpsDataCount - 1; i++) {
-          gpsDataArray[i] = gpsDataArray[i + 1];
-        }
-        gpsDataCount--;
-      }
-    } else {
-      Serial.println("MQTT connection failed!");
+  if (!mqtt.connected() && !mqttConnect()) {
+    Serial.println("MQTT connection failed!");
+    return;
+  }else {
+      mqtt.publish(TOPIC_GPS, gpsDataArray[0].c_str());
+    for (int i = 0; i < gpsDataCount - 1; i++) {
+      gpsDataArray[i] = gpsDataArray[i + 1];
     }
-  } else {
-    //device still connected to mqtt broker
-    if (gpsDataCount > 0) {
-      String message = gpsDataArray[0];
-      mqtt.publish(TOPIC_GPS, message.c_str());
-      for (int i = 0; i < gpsDataCount - 1; i++) {
-        gpsDataArray[i] = gpsDataArray[i + 1];
-      }
-      gpsDataCount--;
-    }
+    gpsDataCount--;
   }
+    
 }
 
-  //spying
+
+
+//spying
 void checkIncomingCall() {
   sim808Serial.println("AT+CLCC");
   delay(1000);
